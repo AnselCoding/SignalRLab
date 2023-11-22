9f3d73b0be47b7572335437910ab4928cebf41f3
Ansel90095 <anselsiao@gmail.com>
2023-11-22 18:01:41 +0800

    Add Controller call hub methods.

---
+++ SignalRLab/Controllers/ApiControllers/CallHubController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRLab.Hubs;
+using System.Security.Claims;
+
+namespace SignalRLab.Controllers.ApiControllers
+{
+    [ApiExplorerSettings(GroupName = "controllers")] //顯示於 Swagger 指定分頁中
+    [Authorize(Policy = "PolicyForPath2")]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CallHubController : ControllerBase
+    {
+        private readonly IHubContext<AllHub> _hubContext;
+
+        public CallHubController(IHubContext<AllHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        // GET: api/CallHub/SendMessage
+        [HttpGet]
+        public async Task<ActionResult> SendMessage(string message)
+        {
+            // 取 ClaimsPrincipal 使用者資料
+            var userId = User.FindFirstValue("Id");
+            await _hubContext.Clients.All.SendAsync("ReceivePodcast", User.FindFirstValue("name"), message);
+            return Ok(userId);
+        }
+    }
+}
